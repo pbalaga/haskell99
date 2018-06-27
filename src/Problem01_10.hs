69d05565fd74902780924e666a4258dee7b7f8c4
pbalaga <ppbalaga@gmail.com>
2018-06-27 22:11:57 +0200

    Solved problems 02, 03

--- src/Problem01_10.hs
+++ src/Problem01_10.hs
@@ -1,6 +1,5 @@
 module Problem01_10
-    ( myLast
-    ) where
+where
 
 -- Problem 01
 myLast :: [a] -> a
@@ -11,3 +10,16 @@ myLast (x:xs) = myLast xs
 myLast' = head . reverse
 
 -- Problem 02
+myButLast :: [a] -> a
+myButLast [] = error "Empty list"
+myButLast [x] = error "List has only one element"
+myButLast [x1,x2] = x1
+myButLast (x:xs) = myButLast xs
+
+-- Problem 02
+elementAt :: (Num k, Eq k) => [a] -> k -> a
+elementAt [] k = error "Too few elements in list"
+elementAt (x:xs) k = 
+    if k==1 
+        then x 
+        else elementAt xs (k-1)
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,7 @@ import Problem01_10
 callAll :: [(d -> a)] -> d -> [a]
 callAll fns x = [fn x | fn <- fns]
 
+
 main :: IO ()
 main = hspec $ do
     describe "01 myLast" $ do
@@ -12,6 +13,27 @@ main = hspec $ do
             myLast [] `shouldThrow` anyErrorCall
             myLast [1..4] `shouldBe` 4
             myLast ['x','y','z'] `shouldBe` 'z'
-            fn "abc" `shouldBe` 'c'
-            where fn = callAll [myLast]
+            myLast "abc" `shouldBe` 'c'
+            -- myLast [] `shouldThrow` anyErrorCall
+            -- myLast [1..4] `shouldBe` 4
+            -- myLast ['x','y','z'] `shouldBe` 'z'
+            -- --fn "abc" `shouldBe` 'c'
+            -- map (shouldBe 'c') (fn "abc")
+            -- where fn = callAll [myLast]
 
+    describe "02 myButLast" $ do
+        it "returns the last but one element of a list" $ do
+            myButLast [] `shouldThrow` anyErrorCall
+            --myButLast [1] `shouldThrow` anyErrorCall
+            myButLast [1..4] `shouldBe` 3
+            myButLast ['x','y','z'] `shouldBe` 'y'
+            myButLast "abc" `shouldBe` 'b'
+
+    describe "03 elementAt" $ do
+        it "returns k-th element of a list" $ do
+            elementAt [] 0 `shouldThrow` anyErrorCall
+            elementAt [] 1 `shouldThrow` anyErrorCall
+            elementAt [1] 1 `shouldBe` 1
+            elementAt [1..4] 3 `shouldBe` 3
+            elementAt [2..7] 5 `shouldBe` 6
+            elementAt "abcdef" 2 `shouldBe` 'b'

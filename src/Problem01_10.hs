608ec37ebb17b726f8fdaeea20a8e0688121cc04
pbalaga <ppbalaga@gmail.com>
2018-07-08 22:16:47 +0200

    Solved problems up to 07

--- src/Problem01_10.hs
+++ src/Problem01_10.hs
@@ -16,10 +16,38 @@ myButLast [x] = error "List has only one element"
 myButLast [x1,x2] = x1
 myButLast (x:xs) = myButLast xs
 
--- Problem 02
+-- Problem 03
 elementAt :: (Num k, Eq k) => [a] -> k -> a
 elementAt [] k = error "Too few elements in list"
 elementAt (x:xs) k = 
     if k==1 
         then x 
         else elementAt xs (k-1)
+
+-- Problem 04
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+-- Problem 05
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = (myReverse xs) ++ [x]
+
+-- Problem 06
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == (reverse xs)
+
+-- Problem 07
+data NestedList a = Elem a | List [NestedList a] | Empty deriving (Show, Eq)
+
+myJoin :: [[a]] -> [a]
+myJoin [] = []
+myJoin [xs] = xs
+--myJoin [xs1,xs2] = xs1 ++ xs2
+myJoin (xs:xss) = xs ++ (myJoin xss)
+
+myFlatten :: NestedList a -> [a]
+myFlatten (Empty) = []
+myFlatten (Elem x) = [x]
+myFlatten (List (xs)) = myJoin (map myFlatten xs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -37,3 +37,33 @@ main = hspec $ do
             elementAt [1..4] 3 `shouldBe` 3
             elementAt [2..7] 5 `shouldBe` 6
             elementAt "abcdef" 2 `shouldBe` 'b'
+
+    describe "04 myLength" $ do
+        it "returns number of elements in a list" $ do
+            myLength [] `shouldBe` 0
+            myLength [1] `shouldBe` 1
+            myLength [5..100] `shouldBe` 96
+            myLength "abcdef" `shouldBe` 6
+
+    describe "05 myReverse" $ do
+        it "reverses a list" $ do
+            myReverse [] `shouldBe` ([] :: [Int])
+            myReverse [1] `shouldBe` [1]
+            myReverse [3,5,7] `shouldBe` [7,5,3]
+            myReverse "abcdef" `shouldBe` "fedcba"
+
+    describe "06 isPalindrome" $ do
+        it "checks if a collection is a palindrome" $ do
+            isPalindrome ([] :: [Int]) `shouldBe` True
+            isPalindrome [1] `shouldBe` True
+            isPalindrome [3,5,7] `shouldBe` False
+            isPalindrome [3,5,7,5,3] `shouldBe` True
+            isPalindrome "ghijkjihg" `shouldBe` True
+
+    describe "07 myFlatten" $ do
+        it "transforms a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively)" $ do
+            myFlatten (Elem 1) `shouldBe` [1]
+            myFlatten (List [Elem 1]) `shouldBe` [1]
+            myFlatten (List [Elem 1, Elem 2, List [Elem 4, List [Elem 5]]]) `shouldBe` [1,2,4,5]
+            --myFlatten (Empty) `shouldBe` []
+            --myFlatten (List [Empty]) `shouldBe` []
